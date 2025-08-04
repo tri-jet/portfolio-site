4db45907732fffe9fe7395e1016c07ff8575623f
tri-jet <sahatrijit123@gmail.com>
2025-08-04 14:36:53 +0100

    chore: configure static export and deployment for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/portfolio-site',
+  assetPrefix: '/portfolio-site',
+
 };
 
 export default nextConfig;
