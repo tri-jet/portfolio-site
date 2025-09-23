9b659775cf1e4984ec386fe75d34c465ead4046d
tri-jet <sahatrijit123@gmail.com>
2025-09-23 13:48:23 +0100

    fix: Add base path to fix relative imports in github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/portfolio-site',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
